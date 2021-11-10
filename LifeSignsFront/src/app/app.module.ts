4a29e192d40d8f0fb27c37d0b2bfd8e179775e7c
Vapidjimbo <56098158+Vapidjimbo@users.noreply.github.com>
2021-11-10 10:56:28 -0800

    Added Modal Component, just HTML so far.

--- LifeSignsFront/src/app/app.module.ts
+++ LifeSignsFront/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DoctorCovidStatusComponent } from './doctor-covid-status/doctor-covid-status.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DoctorCovidStatusComponent
   ],
   imports: [
     BrowserModule
---
+++ LifeSignsFront/src/app/doctor-covid-status/doctor-covid-status.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-doctor-covid-status',
+  templateUrl: './doctor-covid-status.component.html',
+  styleUrls: ['./doctor-covid-status.component.css']
+})
+export class DoctorCovidStatusComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
