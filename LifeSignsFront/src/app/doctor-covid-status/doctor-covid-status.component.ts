31187171a4672b737e3cacaa86b85bef7e5e0a50
Vapidjimbo <56098158+Vapidjimbo@users.noreply.github.com>
2021-11-11 14:27:55 -0800

    Rework of covid survey

--- LifeSignsFront/src/app/doctor-covid-status/doctor-covid-status.component.ts
+++ LifeSignsFront/src/app/doctor-covid-status/doctor-covid-status.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { covidSurvey } from '../services/util/covidSurvey';
 
@@ -9,6 +9,10 @@ import { covidSurvey } from '../services/util/covidSurvey';
 })
 export class DoctorCovidStatusComponent implements OnInit {
   //Fields
+  //Fields
+  @ViewChild("showModal", { static: true })
+  modal!: ElementRef;
+
   covidStatusForm = new FormGroup({
     headache: new FormControl('', Validators.required),
     noSmellTaste: new FormControl('', Validators.required),
@@ -26,31 +30,39 @@ export class DoctorCovidStatusComponent implements OnInit {
     cough: false,
     breathingDifficulties: false,
     exposed: false,
-    hasTraveled: false
+    hasTraveled: false,
   };
 
-  //hasDisplayed = false;
+  hasDisplayed = false;
+  today = new Date().getDay();
 
   //Constructor
-  constructor() {}
+  constructor() {
+
+  }
 
   //Methods
   ngOnInit(): void {
+    if(this.today !== 1) {
+      this.hasDisplayed = false;
+    }
 
-
-      let today: number = new Date().getDay();
-      today === 4 && this.displayModal();
-
+    if(this.today === 1 && this.hasDisplayed === false) {
+      this.displayModal();
+    }
 
   }
 
   //get value selected on submit.
   submit() {
     let formValues = this.covidStatusForm.value;
-    for(let key in formValues) {
-       if(formValues[key] !== "" || formValues[key] !== "no") {
-         //this.covidSurvey[key] = true;
-       }
+    type formKey = "headache" | "noSmellTaste" | "fever" | "cough" | "breathingDifficulties" | "exposed" | "hasTraveled";
+    for (let key in formValues) {
+      if (formValues[key] === "" || formValues[key] === "no") {
+        this.covidSurvey[<formKey>key] = false;
+      } else {
+        this.covidSurvey[<formKey>key] = true;
+      }
     }
 
     console.log(this.covidStatusForm.value);
@@ -61,6 +73,7 @@ export class DoctorCovidStatusComponent implements OnInit {
   //This simulates the button being clicked, the button is hidden in the view.
   //There is probably a better way to do this.
   displayModal() {
-    document.getElementById('showModal')?.click();
+    //document.getElementById('showModal')?.click();
+    this.modal.nativeElement.click();
   }
 }
