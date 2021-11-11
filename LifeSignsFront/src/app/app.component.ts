ece3b1d77619eb0d39fe13a05a3aeef27d750a35
Michael Whitaker <miwhitaker@ymail.com>
2021-11-11 14:25:50 -0600

    added font awesome icon and togglable menu to navbar

--- LifeSignsFront/src/app/app.component.ts
+++ LifeSignsFront/src/app/app.component.ts
@@ -8,4 +8,9 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'LifeSignsFront';
   isVisible = false;
+  toggleUserMenu():void {
+    this.isVisible = !this.isVisible;
+  }
+    
 }
+
