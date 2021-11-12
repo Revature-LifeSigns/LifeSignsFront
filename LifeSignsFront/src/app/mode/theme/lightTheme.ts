ee3cd9d43d82a249d098ee745de41cc7b906a67b
risamorishima <r.morishima@outlook.jp>
2021-11-11 19:13:15 -0500

    changed style colors based on color schema

--- LifeSignsFront/src/app/mode/theme/lightTheme.ts
+++ LifeSignsFront/src/app/mode/theme/lightTheme.ts
@@ -3,7 +3,11 @@ import { Theme } from "./theme";
 export const LightTheme: Theme = {
     themeName: "light",
     styles:{
-        "--bg-color": "#EEEEEE",
-        "--text-color": "#232323"
+        "--bg-color": "white",
+        "--text-color": "black",
+        "--navbar-bg-color": "#474C55",
+        "--navbar-text-color": "white",
+        "--menu-bg-color": "#F26925",
+        "--menu-text-color": "white"
     }
 }
