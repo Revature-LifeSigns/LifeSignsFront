e886822513bfca93aeeeb24a4d107462fd7efaad
Iaroslav Mokroguz <maustrauk@gmail.com>
2021-11-17 14:08:09 -0500

    Survey Service Tests

--- LifeSignsFront/src/app/services/survey/survey.service.spec.ts
+++ LifeSignsFront/src/app/services/survey/survey.service.spec.ts
@@ -1,16 +1,39 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+
 
 import { SurveyService } from './survey.service';
 
 describe('SurveyService', () => {
   let service: SurveyService;
+  let httpMock: HttpTestingController;
+  const dummySurvey: String = JSON.stringify({
+    userId: 1,
+    hasSymptoms: true,
+    isExposed: false,
+    hasTraveled: true,
+  });
+  const urlBase: String = "http://localhost:9025/LifeSigns";
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports:[HttpClientTestingModule],
+      providers:[SurveyService]
+    });
     service = TestBed.inject(SurveyService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should have insertSurvey() return response', () => {
+    service.insertSurvey(dummySurvey).subscribe(response => {
+      expect(response.toString()).toEqual(dummySurvey);
+    })
+    const req = httpMock.expectOne(urlBase + "/survey/insert");
+    expect(req.request.method).toBe("POST");
+    req.flush(dummySurvey);
+  })
 });
