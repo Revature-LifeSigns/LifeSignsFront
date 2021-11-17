752cda56dd906e9d7632d6dbbeb2ef52a30be0d7
Lady Teldra <73320959+sfunk11@users.noreply.github.com>
2021-11-16 20:11:36 -0500

    fixed merge conflicts

--- LifeSignsFront/src/app/profiles/profiles.component.ts
+++ LifeSignsFront/src/app/profiles/profiles.component.ts
@@ -14,14 +14,9 @@ export class ProfilesComponent implements OnInit {
   constructor(private userServ:UserService, private nurseServ:NurseService) { }
 
   ngOnInit(): void {
-<<<<<<< HEAD
-=======
-
->>>>>>> 7537265b119f99bda0343acf1a127fdbac0b3a31
     this.currentUser = this.userServ.getLoggedInUser();
     console.log(this.currentUser);
     this.loadPhoto();
-<<<<<<< HEAD
   }
 
   loadPhoto(){
@@ -31,22 +26,11 @@ export class ProfilesComponent implements OnInit {
         this.currentUser.image = "http://s3.amazonaws.com/lifesigns/" + res.imageFileName;
       }
     )
-<<<<<<< HEAD
-=======
->>>>>>> 3817270e4143de985388f6f808aa15024ac5b460
-=======
->>>>>>> 7537265b119f99bda0343acf1a127fdbac0b3a31
   }
 
-<<<<<<< HEAD
   updatePhoto() {
     alert('clicked update photo')
   }
-=======
-  // updatePhoto() {
-  //   alert('clicked update photo')
-  // }
->>>>>>> 3817270e4143de985388f6f808aa15024ac5b460
 }
 
-}
+
