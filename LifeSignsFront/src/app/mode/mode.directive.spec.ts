2336baaebb09ffec81a53e915ac0ae79155f0bb2
Jessica Shi <jessicaaa.shi@gmail.com>
2021-11-21 13:17:52 -0600

    added test for mode directive

--- LifeSignsFront/src/app/mode/mode.directive.spec.ts
+++ LifeSignsFront/src/app/mode/mode.directive.spec.ts
@@ -1,8 +1,14 @@
+import { ElementRef } from '@angular/core';
+import { ModeService } from '../services/mode/mode.service';
+import { UserService } from '../services/user/user.service';
 import { ModeDirective } from './mode.directive';
 
-// describe('ModeDirective', () => {
-//   it('should create an instance', () => {
-//     const directive = new ModeDirective();
-//     expect(directive).toBeTruthy();
-//   });
-// });
+describe('ModeDirective', () => {
+    let modeServ: ModeService;
+    let userServ: UserService;
+    let elementRef: ElementRef;
+    it('should create an instance', () => {
+        const directive = new ModeDirective(modeServ, userServ, elementRef);
+        expect(directive).toBeTruthy();
+    });
+});
