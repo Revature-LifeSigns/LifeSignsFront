b88da7db16cff3573f58d8937acf320d9a913fa5
Lady Teldra <73320959+sfunk11@users.noreply.github.com>
2021-11-22 11:10:19 -0500

    updated all the backend urls to EC2

--- LifeSignsFront/src/app/services/survey/survey.service.ts
+++ LifeSignsFront/src/app/services/survey/survey.service.ts
@@ -7,8 +7,8 @@ import { covidSurvey } from '../util/covidSurvey';
   providedIn: 'root',
 })
 export class SurveyService {
-  private urlBase = 'http://localhost:9025/LifeSigns';
-  // private urlBase = "http://ec2-18-116-241-177.us-east-2.compute.amazonaws.com:9025/";
+  // private urlBase = 'http://localhost:9025/LifeSigns';
+  private urlBase = "http://3.84.182.36:9025/LifeSigns";
 
   private httpHead = {
     headers: new HttpHeaders({
